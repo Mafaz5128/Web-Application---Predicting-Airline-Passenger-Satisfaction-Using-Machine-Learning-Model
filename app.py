c16a318df17fea28084b2601da39c847562b5630
Mafaz Manzil <90832527+Mafaz5128@users.noreply.github.com>
2025-06-18 20:06:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 import pandas as pd
 import joblib
 import sklearn
-print(sklearn.__version__)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__, static_folder='static')
 
